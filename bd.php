d5129ba115693c522da5d8f198ef166cd8848c96
flaviusmax <62383007+flaviusmax@users.noreply.github.com>
2021-04-26 21:02:39 +0300

    Add files via upload

---
+++ bd.php
@@ -0,0 +1,64 @@
+<?php
+    //open connection to mysql db
+    //$connection = mysqli_connect("localhost","flavi","zzxxzz","channel") or die("Error " . mysqli_error($connection));
+	
+	class Database {
+		
+		 $db_host = 'localhost'; 
+		 $db_user = 'flavi'; 
+		 $db_pass = 'zzxxzz'; 
+		 $db_name = 'channel'; 
+	 
+public function connect()
+    {
+        if(!$this->con)
+        {
+            $myconn = @mysql_connect($this->db_host,$this->db_user,$this->db_pass);
+            if($myconn)
+            {
+                $seldb = @mysql_select_db($this->db_name,$myconn);
+                if($seldb)
+                {
+                    $this->con = true; 
+                    return true; 
+                } else
+                {
+                    return false; 
+                }
+            } else
+            {
+                return false; 
+            }
+        } else
+        {
+            return true; 
+        }
+    }
+		
+		
+		
+		
+
+		
+	// DB disconnect
+	public function disconnect()
+	{
+    if($this->con)
+    {
+        if(@mysql_close())
+        {
+                       $this->con = false; 
+            return true; 
+        }
+        else
+        {
+            return false; 
+        }
+    }
+	}
+		
+		
+	}// end class
+
+	
+?>
---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+include 'bd.php';
+
+
+    //fetch table rows from mysql db
+    $sql = "select * from tabel1";
+  
+
+  $result = mysqli_query($connection, $sql) or die("Eroare " . mysqli_error($connection));
+  
+     //create an array
+    $emparray = array();
+    while($row =mysqli_fetch_assoc($result))
+    {
+        $emparray[] = $row;
+    }
+	
+	
+	//Convert PHP Array to JSON String
+	
+	 echo json_encode($emparray);
+	 
+	 //
+	 
+	     //close the db connection
+    mysqli_close($connection);
+	
+	//creted the file
+	$exportJsonFile = json_encode($emparray);
+    file_put_contents('jsonFile.json', $exportJsonFile);
+	
+	
+  
+  
+?>
