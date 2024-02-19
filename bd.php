83c0a13bc54aaf1ea49a4e2f67a99bf4776a8fcc
Flavius G <62383007+flaviusmax@users.noreply.github.com>
2024-02-19 14:28:00 +0200

    Update bd.php

--- bd.php
+++ bd.php
@@ -1,6 +1,6 @@
 <?php
     //open connection to mysql db
-    $connection = mysqli_connect("localhost","flavi","zzxxzz","channel") or die("Error " . mysqli_error($connection));
+    $connection = mysqli_connect("localhost","flavi","pass","channel") or die("Error " . mysqli_error($connection));
 	
 	
 	/**
